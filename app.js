03a880ee565470bfe569804c131fbe12058f1495
Roshan <roshannair24@gmail.com>
2021-04-13 22:58:40 +0530

    passport-local-mongoose

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
-mongoose.connect("mongodb://localhost:27017/userDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
+
+
+
 
 
 app.set("view engine", "ejs");
@@ -23,6 +22,24 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set("useCreateIndex", true);
+
 const userSchema = new mongoose.Schema({
 
   email: String,
@@ -30,11 +47,15 @@ const userSchema = new mongoose.Schema({
 
 });
 
-
-
-
+userSchema.plugin(passportLocalMongoose);
 
 const UserModel = new mongoose.model("User", userSchema);
+
+passport.use(UserModel.createStrategy());
+
+passport.serializeUser(UserModel.serializeUser());
+passport.deserializeUser(UserModel.deserializeUser());
+
 
 
 app.get("/", function(req, res) {
@@ -52,72 +73,79 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+
+app.get("/secrets", function(req, res) {
+
+  if (req.isAuthenticated()) {
+
+    res.render("secrets");
+
+  } else {
+
+    res.redirect("/login");
+
+  }
+
+});
+
+app.get("/logout",function(req,res){
+
+req.logout();
+
+res.redirect("/");
+
+
+});
+
+
 app.post("/register", function(req, res) {
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
+  UserModel.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
 
-    const newUser = new UserModel({
-      email: req.body.username,
-      password: hash
+    if (err) {
+      console.log(err);
 
-    });
+      res.redirect("/register");
 
-    newUser.save(function(err) {
+    } else {
 
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
+      passport.authenticate("local")(req, res, function() {
 
-    });
+        res.redirect("/secrets");
 
+      });
 
-
+    }
 
   });
-
-
-
 
 });
 
 
 app.post("/login", function(req, res) {
 
-  const passwordvar = req.body.password;
+  const user = new UserModel({
 
+    username: req.body.username,
+    password: req.body.password
 
-  UserModel.findOne({
-    email: req.body.username
-  }, function(err, foundUser) {
+  });
+
+  req.login(user, function(err) {
 
     if (err) {
+
       console.log(err);
+
     } else {
 
-    if(foundUser){
+      passport.authenticate("local")(req, res, function() {
 
-      bcrypt.compare(passwordvar, foundUser.password, function(err, result) {
-          // result == true
+        res.redirect("/secrets");
 
-          if(result===true){
-
-            res.render("secrets");
-
-
-          }
       });
-
-
-
-    }
-
-
-
-
-
 
     }
 
